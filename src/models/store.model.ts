ee029e9d969aeebf77a1d0942ca78e2a8a88c82e
Maxim Kuzin <iskait04@gmail.com>
2022-11-04 00:55:54 +0300

    Исправление форматирования #3

--- src/models/store.model.ts
+++ src/models/store.model.ts
@@ -1,4 +1,4 @@
 export default interface IStore {
-  showNav: boolean,
-  showMenu: boolean,
+  showNav: boolean;
+  showMenu: boolean;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 module.exports = {
-  content: [
-    "./public/index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-        fontFamily: {
-          'montserrat': ['Montserrat Alternates', 'sans-serif'],
-        },
-        screens: {
-          'lg': {'max': '1024px'}
-        }
+      fontFamily: {
+        montserrat: ["Montserrat Alternates", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
