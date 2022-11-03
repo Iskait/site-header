94b99bef20112887eb537d3c517671fb5929d1a9
Maxim Kuzin <iskait04@gmail.com>
2022-11-04 00:57:43 +0300

    Исправление стилей, приведение к mobile-first

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="flex flex-auto bg-gray-50 border border-gray-50 rounded-md lg:w-full"
+    class="flex flex-auto bg-gray-50 border border-gray-50 rounded-md w-full lg:w-fit"
   >
     <input
       type="text"
       placeholder="Поиск по сайту..."
       class="p-3 outline-none border-none bg-inherit flex-1"
     />
-    <button class="px-4 border-l border-gray-300">
+    <button class="px-4 border-l shrink-0 border-gray-300">
       <img src="@/assets/search-icon.svg" alt="search-btn" />
     </button>
   </div>
