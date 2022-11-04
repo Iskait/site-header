7716e31bc5f8fe8f1efd05ed2175a40f67e2dca9
Maxim Kuzin <iskait04@gmail.com>
2022-11-04 23:15:12 +0300

    Добавление переменных цвета #11

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         montserrat: ["Montserrat Alternates", "sans-serif"],
+      },
+      colors: {
+        background: {
+          primary: "rgb(var(--color-background-primary) / <alpha-value>)",
+          secondary: "rgb(var(--color-background-secondary) / <alpha-value>)",
+        },
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
       },
     },
   },
