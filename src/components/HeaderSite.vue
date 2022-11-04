91021cbe96376e7fb2c215bf1b32d8ff8f3496d4
Maxim Kuzin <iskait04@gmail.com>
2022-11-04 15:50:58 +0300

    refactor: Удаление хранилища (#8)
    * Удаление библиотеки Vuex #7
    
    * Удаление регистрации глобального хранилища #7
    
    * Удаление переменной, связанной с хранилищем #7
    
    * Удаление типов хранилища #7
    
    * Удаление глобального хранилища #7

--- src/components/HeaderSite.vue
+++ src/components/HeaderSite.vue
@@ -4,19 +4,25 @@
     class="flex flex-col bg-white p-8 fixed w-full"
   >
     <div
-      class="flex lg:gap-x-24 justify-between lg:items-center flex-col lg:flex-row items-start"
-      :class="{ 'gap-y-10': showMenu }"
+      class="flex lg:gap-x-24 justify-between lg:items-center flex-col lg:flex-row items-start transition-all duration-300"
+      :class="[
+        isMobileMenuOpen
+          ? 'gap-y-10'
+          : isScrolled && !isMobileMenuOpen
+          ? 'gap-y-2'
+          : 'gap-y-0',
+      ]"
     >
       <div class="w-full lg:w-fit flex justify-between">
         <p class="font-montserrat font-medium">ОЛИМП КЛИНИК</p>
-        <MobileMenu @click="toggleShowMenu" />
+        <MobileMenu @click="isMobileMenuOpen = !isMobileMenuOpen" />
       </div>
       <transition name="slide">
-        <SearchInput v-if="!isLaptopScreen ? showMenu : true" />
+        <SearchInput v-if="!isLaptopScreen ? isMobileMenuOpen : true" />
       </transition>
       <div id="mobile-nav" />
       <transition name="slide">
-        <PrivateBlock v-if="!isLaptopScreen ? showMenu : true" />
+        <PrivateBlock v-if="!isLaptopScreen ? isMobileMenuOpen : true" />
       </transition>
     </div>
     <div id="desktop-nav" />
@@ -25,24 +31,23 @@
       :to="isLaptopScreen ? '#desktop-nav' : '#mobile-nav'"
     >
       <transition name="slide">
-        <HeaderNav v-if="isLaptopScreen ? isHovered || isScrolled : showMenu" />
+        <HeaderNav
+          v-if="isLaptopScreen ? isHovered || isScrolled : isMobileMenuOpen"
+        />
       </transition>
     </teleport>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted, onUnmounted } from "vue";
-import { useStore } from "@/store";
+import { ref, onMounted, onUnmounted } from "vue";
 import SearchInput from "./SearchInput.vue";
 import HeaderNav from "./HeaderNav.vue";
 import PrivateBlock from "./PrivateBlock.vue";
-const store = useStore();
 const mount = ref<boolean>(false);
 const isHovered = ref<boolean>(false);
 const isScrolled = ref<boolean>(false);
-const showMenu = computed<boolean>(() => store.state.showMenu);
-const toggleShowMenu = () => store.commit("toggleShowMenu");
+const isMobileMenuOpen = ref<boolean>(false);
 
 const isLaptopScreen = ref(window.screen.availWidth >= 1024);
 
