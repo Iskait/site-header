34c5903c06c0bbebb1de9e92210f0991b45477ec
Maxim Kuzin <iskait04@gmail.com>
2022-11-04 23:16:45 +0300

    Обновление цветов в компонентах #11

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,14 +1,24 @@
 <template>
   <div
-    class="flex flex-auto bg-gray-50 border border-gray-50 rounded-md w-full lg:w-fit"
+    class="flex flex-auto bg-background-primary border-primary border rounded-md w-full lg:w-fit overflow-hidden"
   >
     <input
       type="text"
       placeholder="Поиск по сайту..."
-      class="p-3 outline-none border-none bg-inherit flex-1"
+      class="p-3 outline-none bg-inherit flex-1"
     />
-    <button class="px-4 border-l shrink-0 border-gray-300">
-      <img src="@/assets/search-icon.svg" alt="search-btn" />
+    <button class="px-4 border-l shrink-0 border-primary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        width="20px"
+        height="20px"
+        fill="currentColor"
+      >
+        <path
+          d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"
+        />
+      </svg>
     </button>
   </div>
 </template>
