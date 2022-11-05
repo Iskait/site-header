5d1788724594c08691df713987e58a2749127517
Maxim Kuzin <iskait04@gmail.com>
2022-11-06 01:40:22 +0300

    refactor: Миграция на Vite (#18)
    * Добавление svg иконок #17
    
    * Миграция на Vite, подключение vite-svg-loader #17
    
    * Добавление типов для vite #17
    
    * Использование vite-svg-loader #17

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,9 +1,12 @@
 declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    unknown
-  >;
+  import { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
   export default component;
+}
+
+declare module "*.svg?component" {
+  import { FunctionalComponent, SVGAttributes } from "vue";
+  const src: FunctionalComponent<SVGAttributes>;
+  export default src;
 }
